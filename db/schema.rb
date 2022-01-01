cd64198dbb1b0e8d342c705ba6b6b0b5b38650c8
shunyama <26703177+shunyama@users.noreply.github.com>
2022-01-01 15:02:04 +0900

    scaffold conversation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_055518) do
+ActiveRecord::Schema.define(version: 2022_01_01_060115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2022_01_01_055518) do
     t.text "description"
     t.boolean "disabled", default: false, null: false
     t.integer "created_by"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
